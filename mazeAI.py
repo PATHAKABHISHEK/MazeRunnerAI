b393ab9533ef3746543ae704c19c540b14653f24
Pathak <PATHAKABHISHEK0003@GMAIL.COM>
2020-05-18 16:41:41 +0530

    added Hill Climbing AI for Solving Maze Search Problem

--- mazeAI.py
+++ mazeAI.py
@@ -11,12 +11,34 @@ problem = [
     ["#", "#", "#", "#", " ", "#", "#"],
     ["#", "#", "#", " ", " ", "#", "#"],
     ["#", "#", " ", " ", "#", "#", "#"],
-    ["#", " ", " ", " ", "#", "#", "#"],
+    ["#", " ", "A", " ", "#", "#", "#"],
     ["#", "#", " ", "#", "#", "#", "#"],
     ["#", " ", " ", " ", "#", "#", "#"],
     ["#", " ", "#", " ", "#", "#", "#"],
-    ["A", " ", " ", " ", " ", " ", " "]
+    [" ", " ", " ", " ", " ", " ", " "]
 ]
+
+manhattanDistanceFromGoal = [
+
+]
+
+def calculateManhattanDistanceOfEachPointFromGoal():
+    goalPosition = findGoalPositiom(problem)
+    for i in range(len(problem)):
+        manhattanDistanceFromGoal.append([])
+        for j in range(len(problem[0])):
+            manhattanDistanceFromGoal[i].append(abs(goalPosition[0] - i) + abs(goalPosition[1] - j))
+
+def calculateNodeHavingMinimumManHattan(frontier):
+    node = None
+    minimum = 1000
+    for i in frontier:
+        currentNodeDistance = manhattanDistanceFromGoal[i.currentPosition[0]][i.currentPosition[1]]
+        if(currentNodeDistance < minimum):
+            minimum = currentNodeDistance
+            node = i
+    return node
+
 
 # Actions
 # 1. Up 
@@ -178,7 +200,80 @@ class QueueFrontier:
         
 
 
-# Hill Climbing
+# Hill Climbing (Using Manhattan Distance)
+class HillClimbingFrontier:
+    def __init__(self):
+        self.frontier = []
+        self.isVisitedState = []
+
+    def addNode(self, node):
+        self.frontier.append(node)
+    
+    def checkIfStateVisited(self, node):
+        if(node in self.isVisitedState):
+            return True
+        return False
+    
+    def removeNode(self):
+        node = calculateNodeHavingMinimumManHattan(self.frontier)
+        # print("")
+        # print("New")
+        # for k in self.frontier:
+        #     for i in range(len(k.state)):
+        #         for j in range(len(k.state[0])):
+        #             print(k.state[i][j], end="\t")
+        #         print("\n")
+        if findGoalPositiom(problem) == node.currentPosition:
+            global notReachedGoalState
+            global states
+            global actions
+            print("Reached Goal State")
+            notReachedGoalState = False
+            while node:
+                states.append(node.state)
+                actions.append(node.action)
+                node = node.parent
+            states = states[: -1]
+            actions = actions[: -1]
+            states.reverse()
+            actions.reverse()
+            
+
+
+        else:
+            self.isVisitedState.append(node.state)
+            if len(self.frontier) > 1:
+                self.frontier.remove(node)
+            else:
+                self.frontier = []
+            self.expandNode(node)
+    
+    def expandNode(self, node):
+        position = node.currentPosition
+        if (position[0] >= 0 and not position[0] == len(problem) - 1) and (problem[position[0] + 1][position[1]] == " " or problem[position[0] + 1][position[1]] == "B"):
+            smallProblem = copy.deepcopy(problem)
+            smallProblem[position[0] + 1][position[1]] = '*'
+            if(not self.checkIfStateVisited(smallProblem)):
+                self.addNode(Node(smallProblem, node, "down", [position[0] + 1, position[1]]))
+        if (position[0] <= len(problem) - 1 and not position[0] == 0) and (problem[position[0] - 1][position[1]] == " " or problem[position[0] - 1][position[1]] == "B"):
+            # check for up
+            smallProblem = copy.deepcopy(problem)
+            smallProblem[position[0] - 1][position[1]] = '*'
+            if(not self.checkIfStateVisited(smallProblem)):
+                self.addNode(Node(smallProblem, node, "up", [position[0] - 1, position[1]]))
+        if (position[1] >= 0 and not position[1] == len(problem[0]) - 1) and (problem[position[0]][position[1] + 1] == " " or problem[position[0]][position[1] + 1] == "B"):
+            # check for right
+            smallProblem = copy.deepcopy(problem)
+            smallProblem[position[0]][position[1] + 1] = '*'
+            if(not self.checkIfStateVisited(smallProblem)):
+                self.addNode(Node(smallProblem, node, "right", [position[0], position[1] + 1]))
+        if (position[1] <= len(problem[0]) - 1 and not position[1] == 0) and (problem[position[0]][position[1] - 1] == " " or problem[position[0]][position[1] - 1] == "B"):
+            # check for left
+            smallProblem = copy.deepcopy(problem)
+            smallProblem[position[0]][position[1] - 1] = '*'
+            if(not self.checkIfStateVisited(smallProblem)):
+                self.addNode(Node(smallProblem, node, "left", [position[0], position[1] - 1]))
+        
 
 
 
@@ -198,7 +293,8 @@ def findGoalPositiom(problem):
     return [0, len(problem[0]) - 1]
 
 myNode = Node(problem, None, None, findInitialCurrentPosition(problem))
-myFrontier = QueueFrontier()
+calculateManhattanDistanceOfEachPointFromGoal()
+myFrontier = HillClimbingFrontier()
 myFrontier.addNode(myNode)
 while(notReachedGoalState):
     myFrontier.removeNode()
@@ -206,3 +302,11 @@ while(notReachedGoalState):
 
 for i in actions:
     print(i)
+
+print("")
+print("Manhattan Distance")
+print("")
+for i in range(len(problem)):
+    for j in range(len(problem[0])):
+        print(manhattanDistanceFromGoal[i][j], end="\t")
+    print("")
